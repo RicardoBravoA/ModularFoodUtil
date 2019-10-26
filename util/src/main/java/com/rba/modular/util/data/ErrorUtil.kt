cc28f71699b80ab5c8bbbc7c2bf072ab0fb21d3d
Ricardo Bravo <=>
2019-10-25 23:05:33 -0500

    chore: initial commit

---
+++ util/src/main/java/com/rba/modular/util/data/ErrorUtil.kt
@@ -0,0 +1,23 @@
+package com.rba.modular.util.data
+
+import com.rba.modular.model.model.ErrorModel
+
+object ErrorUtil {
+
+    fun errorHandler(error: Throwable): ErrorModel {
+
+        val errorException: RetrofitException =
+            if (error is RetrofitException) {
+                error
+            } else {
+                RetrofitException.retrofitException(error)
+            }
+
+        return when (errorException.kind) {
+            RetrofitException.Kind.HTTP -> errorException.getErrorBodyAs(ErrorModel::class.java)!!
+            RetrofitException.Kind.NETWORK -> ErrorModel()
+            else -> ErrorModel(0, "", "Ocurrió un error")
+        }
+
+    }
+}
